40454b8e2ba9da37ec8d061cfc7b7c94b99e5307
huangcongyu2006 <huangcongyu2006@4eeea0a6-3caa-11de-801c-c7cb65f9a556>
2009-05-11 03:48:17 +0000

    v0.1

---
+++ Mess/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: hcy
+ *
+ * Created on 2009年5月11日, 上午11:47
+ */
+
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+    cout<<"hello cpp"<<endl;
+    return (EXIT_SUCCESS);
+}
+
